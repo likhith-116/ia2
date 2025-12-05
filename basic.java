06fb354b3eba32db58f769237a729c829bd16303
likhith-116 <vvce23cse0155@vvce.ac.in>
2025-12-05 12:18:52 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public satic void main(String[] args){
-    System.out.println("Hello");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
